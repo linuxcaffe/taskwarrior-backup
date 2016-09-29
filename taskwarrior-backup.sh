c05bf91bcb5d7cf79a381b8155a637444923e7c4
David J Patrick <davamundo@gmail.com>
2016-09-28 21:29:41 -0400

    Update taskwarrior-backup.sh

--- taskwarrior-backup.sh
+++ taskwarrior-backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Create an archive of the Taskwarrior $TASK_DIR
+TASK_DIR=~/.task/
 # Backups are kept in $TASK_BACKUP_DIR
+TASK_BACKUP_DIR=~/backup/
+#
 cd $TASK_BACKUP_DIR && tar -czf task-backup-$(date +'%Y%m%d%H%M%S').tar.gz -C $TASK_DIR .
